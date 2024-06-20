f0be91685c9032deedcd11840c5a5adbd69bf553
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2024-06-20 11:25:45 -0300

    teste build

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo "Instalando pacote java ${JAR_FILENAME}"
 echo "Verificando variável de treinamento ${TRAINING}"
 
 echo "java -jar ${JAR_FILENAME} -console -url="jdbc:postgresql://psql:5432/${POSTGRES_DATABASE}" -username ${POSTGRES_USERNAME} -password ${POSTGRES_PASSWORD} ${TRAINING}"
-java -jar ${JAR_FILENAME} -console -url="jdbc:postgresql://psql:5432/${POSTGRES_DATABASE}" -username ${POSTGRES_USERNAME} -password ${POSTGRES_PASSWORD} ${TRAINING}
+java -jar eSUS-AB-PEC-5.2.36-Linux64.jar -console -url="jdbc:postgresql://psql:5432/esus" -username postgres -password esus -treinamento
